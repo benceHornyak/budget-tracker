bece4d2a5b3e5f9da5c8fd5414fb4a7a0b57a6e2
Bence Hornyák <sir.bence15@gmail.com>
2023-03-07 21:26:13 +0100

    feat: #3 create initial setup for auth

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,13 @@
+import { signOut } from 'next-auth/react';
 import Link from 'next/link';
+import { Button } from '../button/button';
 import { Logo } from '../logo/logo';
 
 export const Header = (): JSX.Element => {
+  const onLogoutButtonClick = (): void => {
+    signOut();
+  };
+
   return (
     <>
       <div className="flex gap-3 items-center h-[10%] shadow-md justify-between px-5">
@@ -18,6 +24,7 @@ export const Header = (): JSX.Element => {
           <Link className="text-2xl" href="/auth">
             Sign in
           </Link>
+          <Button onClick={onLogoutButtonClick}>Logout</Button>
         </div>
       </div>
     </>
--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -2,15 +2,20 @@ import { Button } from '@budget-tracker/components/button/button';
 import { Card } from '@budget-tracker/components/card/card';
 import { Logo } from '@budget-tracker/components/logo/logo';
 import Link from 'next/link';
+import { signIn } from 'next-auth/react';
 
 const Auth = (): JSX.Element => {
+  const onLoginButtonClick = (): void => {
+    signIn('google', { callbackUrl: '/' });
+  };
+
   return (
     <>
       <Card>
         <Link href="/">
           <Logo />
         </Link>{' '}
-        <Button onClick={(): void => console.log('hello')} icon={'icon-google'}>
+        <Button onClick={onLoginButtonClick} icon={'icon-google'}>
           Continue with Google
         </Button>
       </Card>
